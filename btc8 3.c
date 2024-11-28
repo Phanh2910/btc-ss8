ecc6d6d097dcb9efb390f8a3e6f12b21461213c7
Phanh2910 <phanphuocanh97@gmail.com>
2024-11-29 01:01:38 +0700

    Update btc8 3.c

--- btc8 3.c
+++ btc8 3.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-
 int main() {
     int a, i, j;
     printf("nhap so bat ky: ");
     scanf("%d", &a);
-    int ar[a][a];
-    for (i = 0; i < a; i++) 
-    for (j = 0; j < a; j++) {
+    int arr[a][a];
+    for (i=0;i<a;i++) {
+           for (j=0;j<a;j++) {
             printf("So hang la [%d][%d]: ", i, j);
             scanf("%d", &ar[i][j]);
-        }
-    for (i = 0; i < a; i++) {
-    for (j = 0; j < a; j++) 
-            printf("%d ", ar[i][j]);
-        printf("\n");
+                                         }
+    for (i=0;i<a;i++) {
+            for (j=0;j<a;j++) {
+              printf("%d ", arr[i][j]);
+             printf("\n"); 
+            }         
+     }
     }
-
     return 0;
 }
